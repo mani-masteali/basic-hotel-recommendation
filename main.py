a966496823532bacd0b41287ea35ce7d183b0b05
Mani <mani.masteali7@gmail.com>
2024-11-23 14:38:28 -0500

    database created

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ class Database:
         self.db=db
         self.cursor=cursor
     def create_table(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS hotels ")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS hotels (name text PRIMARY KEY, location text, star real, rate real, location_rate real, price real, sustainable bool, centre_distance real)")
+        self.db.commit()
+        self.db.close()
 
 bridge=sqlite3.connect('data.db')
 cursor=bridge.cursor()
 db=Database(bridge,cursor)
-
-
-bridge.commit()
-bridge.close()
+db.create_table()
